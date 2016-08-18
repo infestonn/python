b929261c781919b2b7bea09ed5bfb8aba4b0e113
yuriyk <yuriyk@netvertise.com>
2016-08-18 16:55:10 +0300

    add Ls.py

---
+++ Programming_in_Python3/Examples/Ls.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import optparse
+import os
+import time
+import locale
+locale.setlocale(locale.LC_ALL, "")
+
+def main():
+	parser = optparse.OptionParser("Usage: ls.py [options] [path1 [path2 [... pathN]]]")
+	parser.add_option("-H", "--hidden", action="store_true", dest="hidden",
+						help='show hidden files')
+	parser.add_option("-m", "--modified", action="store_true", dest="modified",
+						help='show last modified date/time')
+	parser.add_option("-O", "--order", type='choice', choices=['name', 'n', 'modified', 'm', 'size', 's'], dest="order", default="name",
+						help="order by ('name', 'n', 'modified', 'm', 'size', 's')[default: name]")
+	parser.add_option("-r", "--recursive", action="store_true", dest="recursive",
+						help='recurse into subdirectories [default: off]')
+	parser.add_option("-s", "--size", action="store_true", dest="size",
+						help='show sizes [default: off]')
+	opts, args = parser.parse_args()
+	if not args:
+		args = ['./']
+	else:
+		for i in args:
+			if not i.endswith('/'):
+				args[args.index(i)] += "/"
+	print(args)
+
+	if opts.order in ['m', 'modified']:
+		mod = 'date'
+	elif opts.order in ['s', 'size']:
+		mod = 'size'
+	else:
+		mod = 'name'
+	line_keys = []
+	if not opts.recursive:
+		for n in args:
+			for i in os.listdir(n):
+				if opts.hidden:
+					info = {
+						'name': i,
+						'size': (os.path.getsize(n+i)),
+						'date': (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(os.path.getmtime(n+i))))
+					}
+					line_keys.append(info)
+				else:
+					if i.startswith('.'):
+						continue
+					else:
+						info = {
+						'name': i,
+						'size': (os.path.getsize(n+i)),
+						'date': (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(os.path.getmtime(n+i))))
+						}
+						line_keys.append(info)
+	else:	
+		for i in args:
+			for root, dirs, files in os.walk(i):
+				for n in dirs:
+					info = {
+					'name': root + n,
+					'size': (os.path.getsize(root + (n if root.endswith('/') else '/'+n))), 
+					'date': (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(os.path.getmtime(root + (n if root.endswith('/') else '/'+n)))))
+					}
+					line_keys.append(info)
+				for n in files:
+					info = {
+					'name': root + n,
+					'size': (os.path.getsize(root + (n if root.endswith('/') else '/'+n))), 
+					'date': (time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(os.path.getmtime(root + (n if root.endswith('/') else '/'+n)))))
+					}
+					line_keys.append(info)
+				print(root)
+				print(dirs)
+				print(files)
+	# print(line_keys)
+
+	if line_keys:
+		max_size = len(str(max(line_keys, key=lambda x: x['size']).get('size')))
+	else:
+		max_size = 0
+	if opts.size and opts.modified:
+		print_format = '{date} {size:>{ln}n} {name}'
+	elif opts.size:
+		print_format = '{size:>{ln}n} {name}'
+	elif opts.modified:
+		print_format = '{date} {name}'
+	else:
+		print_format = '{name}'
+	for i in sorted(line_keys, key=lambda x: x[mod]):
+		print(print_format.format(**i,ln=max_size+1))
+main()
